f96c5cf7b0ff70bbb8be97d9f4c10e0df80be9c2
Maciej Andrejczuk <maciej.andrejczuk@gmail.com>
2010-04-07 12:02:52 +0200

    Task 3 closed, but yet not finished

--- 3_quatseq/quatseq.cc
+++ 3_quatseq/quatseq.cc
@@ -5,51 +5,81 @@
 
 using namespace std;
 
+#ifdef DEBUG_LEVEL
+    static const int debuglevel = DEBUG_LEVEL;
+#else
+    static const int debuglevel = 0;
+#endif
+
+class Debug {
+    public:
+    int verbose_debuglevel;
+
+    template <class T>
+    Debug& operator<< (const T& message) {
+       if (debuglevel >= verbose_debuglevel)
+            cerr << message;
+        return *this;
+    }
+
+    Debug& operator<< (ostream& (*fun)(ostream&)) {
+        if (debuglevel >= verbose_debuglevel)
+            cerr << fun;
+        return *this;
+    }
+};
+
+static Debug& debug(int dbglevel) {
+    static Debug db;
+    db.verbose_debuglevel = dbglevel;
+    if (debuglevel >= dbglevel)
+        cerr << "quatseq: ";
+    return db;
+}
+
+// Quaternion
+
 Quaternion::Quaternion(double a_, double b_, double c_, double d_)
 : a(a_), b(b_), c(c_), d(d_)
 {
+    debug(2) << "Constructor" << endl;
 }
 
-Quaternion::Quaternion(const Quaternion& q)
+Quaternion::Quaternion(Quaternion const& q)
 : a(q.r()), b(q.i()), c(q.j()), d(q.k())
 {
+    debug(2) << "Copy constructor" << endl;
 }
 
-inline double Quaternion::r() const {
+double const Quaternion::r() const {
     return a;
 }
 
-inline double Quaternion::i() const {
+double const Quaternion::i() const {
     return b;
 }
 
-inline double Quaternion::j() const {
+double const Quaternion::j() const {
     return c;
 }
 
-inline double Quaternion::k() const {
+double const Quaternion::k() const {
     return d;
 }
 
-const Quaternion& Quaternion::operator= (const Quaternion& q) {
+Quaternion const& Quaternion::operator= (Quaternion const& q) {
+    debug(2) << "Operator=" << endl;
     if (this != &q) {
-	a = q.r();
-	b = q.i();
+	    a = q.r();
+	    b = q.i();
         c = q.j();
         d = q.k();
     }
     return *this;
 }
 
-Quaternion& Quaternion::operator- () {
-    a = -a;
-    b = -b;
-    c = -c;
-    d = -d;
-    return *this;
-}
-
-Quaternion& Quaternion::operator+= (const Quaternion& q) {
+Quaternion const& Quaternion::operator+= (Quaternion const& q) {
+    debug(2) << "Operator+=" << endl;
     a += q.r();
     b += q.i();
     c += q.j();
@@ -57,7 +87,8 @@ Quaternion& Quaternion::operator+= (const Quaternion& q) {
     return *this;
 }
 
-Quaternion& Quaternion::operator-= (const Quaternion& q) {
+Quaternion const& Quaternion::operator-= (Quaternion const& q) {
+    debug(2) << "Operator-=" << endl;
     a -= q.r();
     b -= q.i();
     c -= q.j();
@@ -65,7 +96,8 @@ Quaternion& Quaternion::operator-= (const Quaternion& q) {
     return *this;
 }
 
-Quaternion& Quaternion::operator*= (const Quaternion& q) {
+Quaternion const& Quaternion::operator*= (Quaternion const& q) {
+    debug(2) << "Operator*=" << endl;
     double pa = a * q.r() - b * q.i() - c * q.j() - d * q.k();
     double pb = a * q.i() + b * q.r() + c * q.k() - d * q.j();
     double pc = a * q.j() - b * q.k() + c * q.r() + d * q.i();
@@ -77,49 +109,227 @@ Quaternion& Quaternion::operator*= (const Quaternion& q) {
     return *this;
 }
 
-inline Quaternion operator+ (const Quaternion& p, const Quaternion& q) {
+Quaternion const operator+ (Quaternion const& p, Quaternion const& q) {
+    debug(2) << "Operator+" << endl;
     Quaternion r(p);
     return r += q;
 }
 
-inline Quaternion operator- (const Quaternion& p, const Quaternion& q) {
+Quaternion const operator- (Quaternion const& p, Quaternion const& q) {
+    debug(2) << "Operator- (binary)" << endl;
     Quaternion r(p);
     return r -= q;
 }
 
-inline Quaternion operator* (const Quaternion& p, const Quaternion& q) {
+Quaternion const operator* (Quaternion const& p, Quaternion const& q) {
+    debug(2) << "Operator*" << endl;
     Quaternion r(p);
     return r *= q;
 }
 
-inline bool operator== (const Quaternion& p, const Quaternion& q) {
-    if (p.r() != q.r()) return false;
-    if (p.i() != q.i()) return false;
-    if (p.j() != q.j()) return false;
-    return (p.k() == q.k());
+Quaternion const operator- (Quaternion const& p) {
+    debug(2) << "Operator- (unary)" << endl;
+    Quaternion r(0);
+    return r -= p;
 }
 
-inline bool operator!= (const Quaternion& p, const Quaternion& q) {
-    return !operator==(p, q);
+bool operator== (Quaternion const& p, Quaternion const& q) {
+    debug(2) << "Operator==" << endl;
+    return (p.r() == q.r() && p.i() == q.i()
+        && p.j() == q.j() && p.k() == q.k());
 }
 
-inline Quaternion re(const Quaternion& q) {
+bool operator!= (Quaternion const& p, Quaternion const& q) {
+    debug(2) << "Operator!=" << endl;
+    return !(p == q);
+}
+
+ostream& operator<< (ostream& out, Quaternion const& q) {
+    out << "(" << q.r() << "," << q.i() << "," << q.j()
+        << "," << q.k() << ")";
+    return out;
+}
+
+Quaternion const re(Quaternion const& q) {
     return Quaternion(q.r(), 0, 0, 0);
 }
 
-inline Quaternion im(const Quaternion& q) {
+Quaternion const im(Quaternion const& q) {
     return Quaternion(0, q.i(), q.j(), q.k());
 }
 
-inline Quaternion conj(const Quaternion& q) {
+Quaternion const conj(Quaternion const& q) {
     return Quaternion(q.r(), -q.i(), -q.j(), -q.k());
 }
 
-inline static double sqr(double d) {
+static double sqr(double d) {
     return d*d;
 }
 
-inline double norm(const Quaternion& q) {
+double norm(Quaternion const& q) {
     return sqrt(sqr(q.r()) + sqr(q.i()) + sqr(q.j()) + sqr(q.k()));
+}
+
+Quaternion const Zero() {
+    static Quaternion ret(0, 0, 0, 0);
+    return ret;
+}
+
+Quaternion const I() {
+    static Quaternion ret(0, 1, 0, 0);
+    return ret;
+}
+
+Quaternion const J() {
+    static Quaternion ret(0, 0, 1, 0);
+    return ret;
+}
+
+Quaternion const K() {
+    static Quaternion ret(0, 0, 0, 1);
+    return ret;
+}
+
+// QuaternionSequence
+
+QuaternionSequence::QuaternionSequence()
+: data()
+{
+}
+
+QuaternionSequence::QuaternionSequence(map_type const& m)
+: data(m)
+{
+    removeNullQuaternions();
+}
+
+QuaternionSequence::QuaternionSequence(std::vector<Quaternion> const& v)
+{
+    for (int i = 0; i < (int) v.size(); i++) {
+        if (v[i] != Quaternion(0))
+            data[i] = v[i];
+    }
+}
+
+QuaternionSequence::QuaternionSequence(QuaternionSequence const& q)
+: data(q.data)
+{
+    removeNullQuaternions();
+}
+
+QuaternionSequence const& QuaternionSequence::operator= (
+        QuaternionSequence const& q)
+{
+    data = q.data; // czy mapa sama robi sprzątanie?
+    removeNullQuaternions();
+    return *this;
+}
+
+QuaternionSequence const& QuaternionSequence::operator-= (
+        QuaternionSequence const& q)
+{
+    for (map_type::const_iterator ii = q.data.begin(); ii != q.data.end(); ii++)
+    {
+        Quaternion val = (*this)[ii->first] - ii->second;
+        this->data[ii->first] = val;
+    }
+    removeNullQuaternions();
+    return *this;
+}
+QuaternionSequence const& QuaternionSequence::operator+= (
+        QuaternionSequence const& q)
+{
+    for (map_type::const_iterator ii = q.data.begin(); ii != q.data.end(); ii++)
+    {
+        Quaternion val = (*this)[ii->first] + ii->second;
+        this->data[ii->first] = val;
+    }
+    removeNullQuaternions();
+    return *this;
+}
+QuaternionSequence const& QuaternionSequence::operator*= (
+        QuaternionSequence const& q)
+{
+    for (map_type::const_iterator ii = q.data.begin(); ii != q.data.end(); ii++)
+    {
+        Quaternion val = (*this)[ii->first] * ii->second;
+        this->data[ii->first] = val;
+    }
+    removeNullQuaternions();
+    return *this;
+}
+
+Quaternion const QuaternionSequence::operator[] (int k) const
+{
+    map_type::const_iterator ii = data.find(k);
+    if (ii == data.end())
+        return Quaternion(0);
+    return ii->second;
+}
+
+Quaternion& QuaternionSequence::operator[] (int k)
+{
+    map_type::iterator ii = data.find(k);
+    if (ii == data.end()) {
+        data[k] = Zero();
+        return data[k];
+        // The condition 'non-zero elements' is not kept
+    }
+    return ii->second;
+}
+
+QuaternionSequence const operator- (QuaternionSequence const& p,
+        QuaternionSequence const& q)
+{
+    QuaternionSequence r(p);
+    return r -= q;
+}
+
+QuaternionSequence const operator+ (QuaternionSequence const& p,
+        QuaternionSequence const& q)
+{
+    QuaternionSequence r(p);
+    return r += q;
+}
+
+QuaternionSequence const operator* (QuaternionSequence const& p,
+        QuaternionSequence const& q)
+{
+    QuaternionSequence r(p);
+    return r *= q;
+}
+
+void QuaternionSequence::removeNullQuaternions()
+{
+    Quaternion emp(0);
+    for (QuaternionSequence::map_type::iterator ii = data.begin();
+            ii != data.end(); ii++) {
+        if (ii->second == emp)
+            data.erase(ii); // ii is still OK?
+    }
+}
+
+bool operator== (QuaternionSequence const& p,
+        QuaternionSequence const& q)
+{
+    debug(1) << "Operator ==" << endl << p << q;
+    return p.data == q.data;
+}
+
+bool operator!= (QuaternionSequence const& p,
+        QuaternionSequence const& q)
+{
+    debug(1) << "Operator !=" << endl << p << q;
+    return !(p == q);
+}
+
+std::ostream& operator<< (std::ostream& out, QuaternionSequence const& q)
+{
+    out << "QuaternionSequence: " << endl;
+    for (QuaternionSequence::map_type::const_iterator ii = q.data.begin();
+            ii != q.data.end(); ii++) {
+        out << ii->first << ": " << ii->second << endl;
+    }
+    return out;
 }
 
--- 3_quatseq/quatseq.h
+++ 3_quatseq/quatseq.h
@@ -1,50 +1,103 @@
 #ifndef _QUATSEQ_H_
 #define _QUATSEQ_H_
 
+#include <ostream>
+#include <map>
+#include <vector>
+
 class Quaternion
 {
-    private:
-        double a, b, c, d;
     public:
         // Constructor. Creates quaternion (a, b, c, d)
         Quaternion(double a = 0, double b = 0, double c = 0, double d = 0);
         // Copy constructor
-        Quaternion(const Quaternion&);
+        Quaternion(Quaternion const&);
         // Returns first coordinate of quaternion
-        double r() const;
+        double const r() const;
         // Returns second coordinate of quaternion
-        double i() const;
+        double const i() const;
         // Returns third coordinate of quaternion
-        double j() const;
+        double const j() const;
         // Returns forth coordinate of quaternion
-        double k() const;
-        
-        const Quaternion& operator= (const Quaternion&);
-        Quaternion& operator- ();
-        Quaternion& operator+= (const Quaternion&);
-        Quaternion& operator-= (const Quaternion&);
-        Quaternion& operator*= (const Quaternion&);
+        double const k() const;
+
+        Quaternion const& operator= (Quaternion const&);
+        Quaternion const& operator-= (Quaternion const&);
+        Quaternion const& operator+= (Quaternion const&);
+        Quaternion const& operator*= (Quaternion const&);
+    private:
+        double a, b, c, d;
 };
 
-Quaternion operator+ (const Quaternion&, const Quaternion&);
-Quaternion operator- (const Quaternion&, const Quaternion&);
-Quaternion operator* (const Quaternion&, const Quaternion&);
-
-bool operator== (const Quaternion&, const Quaternion&);
-bool operator!= (const Quaternion&, const Quaternion&);
+Quaternion const operator- (Quaternion const&);
+Quaternion const operator- (Quaternion const&, Quaternion const&);
+Quaternion const operator+ (Quaternion const&, Quaternion const&);
+Quaternion const operator* (Quaternion const&, Quaternion const&);
+bool operator== (Quaternion const&, Quaternion const&);
+bool operator!= (Quaternion const&, Quaternion const&);
+std::ostream& operator<< (std::ostream&, Quaternion const&);
 
 // Returns quaternion equal to real part of given quaternion
-Quaternion re(const Quaternion&);
-
+Quaternion const re(Quaternion const&);
 // Returns quaternion equal to imaginary part of given quaternion
-Quaternion im(const Quaternion&);
-
+Quaternion const im(Quaternion const&);
 // Returns conjugation of given quaternion
-Quaternion conj(const Quaternion&);
+Quaternion const conj(Quaternion const&);
+// Returns norm of given quaternion
+double norm(Quaternion const&);
+// Returns quaternion (0, 0, 0, 0)
+Quaternion const Zero();
+// Returns quaternion (0, 1, 0, 0)
+Quaternion const I();
+// Returns quaternion (0, 0, 1, 0)
+Quaternion const J();
+// Returns quaternion (0, 0, 0, 1)
+Quaternion const K();
 
-// Returns norm of iven quaternion
-double norm(const Quaternion&);
 
+class QuaternionSequence
+{
+    public:
+        typedef unsigned int size_type;
+        typedef std::map<size_type, Quaternion> map_type;
+
+        // Creates an empty sequence
+        QuaternionSequence();
+        // Creates a sequence from map
+        QuaternionSequence(map_type const&);
+        // Creates a sequence from vector
+        QuaternionSequence(std::vector<Quaternion> const&);
+        // Copy constructor
+        QuaternionSequence(QuaternionSequence const&);
+
+        QuaternionSequence const& operator= (QuaternionSequence const&);
+        QuaternionSequence const& operator-= (QuaternionSequence const&);
+        QuaternionSequence const& operator+= (QuaternionSequence const&);
+        QuaternionSequence const& operator*= (QuaternionSequence const&);
+        Quaternion const operator[] (int k) const;
+        Quaternion& operator[] (int k);
+        friend bool operator== (QuaternionSequence const&,
+            QuaternionSequence const&);
+        friend std::ostream& operator<< (std::ostream&,
+            QuaternionSequence const&);
+    private:
+        map_type data;
+        void removeNullQuaternions();
+        // Safe bool
+        typedef void (Quaternion::*bool_type)() const;
+        void this_type_does_not_support_comparisons() const {}
+};
+
+QuaternionSequence const operator- (QuaternionSequence const&,
+        QuaternionSequence const&);
+QuaternionSequence const operator+ (QuaternionSequence const&,
+        QuaternionSequence const&);
+QuaternionSequence const operator* (QuaternionSequence const&,
+        QuaternionSequence const&);
+bool operator== (QuaternionSequence const&,
+        QuaternionSequence const&);
+bool operator!= (QuaternionSequence const&,
+        QuaternionSequence const&);
 
 #endif
 
